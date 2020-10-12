be53f4832ffd03beec131edbf1678431ed589107
Charlie Groves <cgroves@fitbit.com>
2020-10-12 11:24:32 -0400

    Add docker compose to stand up postgrest

--- recreate_db.sh
+++ recreate_db.sh
@@ -1,4 +1,5 @@
 set -o errexit
+export PGPASSWORD=RcJhCBt2CE2dz7#B
 
 if [ ! -f bootstrap/sensorpush/csv_to_sql.py ] ; then
   echo "recreate_db.sh must be run from the root of seeries. See 'Bootstrap' in README.md for directions"
@@ -13,21 +14,21 @@ done
 python3 bootstrap/tempest/json_to_sql.py > bootstrap/generated_sql/tempest.sql
 
 echo "-- Nuking and restarting container"
-docker kill timescaledb
-docker rm timescaledb
-docker run -d --name timescaledb -p 127.0.0.1:5432:5432 -e POSTGRES_PASSWORD=password timescale/timescaledb:latest-pg12
+ # --force because we don't want to be prompted, -v to remove the anonymous volumes we attach to postgres
+docker-compose rm --stop --force -v
+docker-compose up --detach
 
 echo "-- Waiting for postgres server" 
 RETRIES=5
-until PGPASSWORD=password psql -h localhost -U postgres -p 5432 -c "select 1" > /dev/null 2>&1 || [ $RETRIES -eq 0 ]; do
+until psql -h localhost -U seeries -p 5432 -c "select 1" > /dev/null 2>&1 || [ $RETRIES -eq 0 ]; do
   echo "$((RETRIES--)) remaining attempts..."
   sleep 1
 done
 
 echo "-- Creating schema"
-PGPASSWORD=password psql -h localhost -p 5432 -U postgres -f src/sql/CREATE.sql 
+psql -h localhost -p 5432 -U seeries -f src/sql/CREATE.sql 
 
 for f in bootstrap/generated_sql/*.sql ; do
   echo "-- Loading $f"
-  PGPASSWORD=password psql -h localhost -p 5432 -U postgres -f $f
+  psql -h localhost -p 5432 -U seeries -f $f
 done
