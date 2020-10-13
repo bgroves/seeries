9857c17a95d37da963ed67e1796166a043aaed11
Charlie Groves <cgroves@fitbit.com>
2020-10-13 05:13:14 -0400

    Switch to compose to make it easier not to depend on a local psql

--- recreate_db.sh
+++ recreate_db.sh
@@ -5,29 +5,38 @@ if [ ! -f bootstrap/sensorpush/csv_to_sql.py ] ; then
   exit 1
 fi
 
-echo "-- Generating data SQL"
-for f in bootstrap/sensorpush/exports/*.csv; do
-  LOCATION=`basename -s .csv $f`
-  python3 bootstrap/sensorpush/csv_to_sql.py $LOCATION $f > bootstrap/generated_sql/$LOCATION.sql
-done
-python3 bootstrap/tempest/json_to_sql.py > bootstrap/generated_sql/tempest.sql
+generate_sql() {
+  echo "-- Generating data SQL"
+  mkdir -p db/target/initdb
+  # Start at 100 as the postgres container runs these in 
+  # order(https://github.com/docker-library/docs/blob/master/postgres/README.md#initialization-scripts) 
+  # and the timescale container we're basing off of adds three scripts starting with 0:
+  # https://github.com/timescale/timescaledb-docker/tree/master/docker-entrypoint-initdb.d 
+  cp src/sql/CREATE.sql db/target/initdb/100_CREATE.sql
+  COUNT=101
+  for f in bootstrap/sensorpush/exports/*.csv; do
+    LOCATION=`basename -s .csv $f`
+    python3 bootstrap/sensorpush/csv_to_sql.py $LOCATION $f > db/target/initdb/${COUNT}_${LOCATION}.sql
+    ((COUNT=COUNT+1))
+  done
+  python3 bootstrap/tempest/json_to_sql.py > db/target/initdb/${COUNT}_tempest.sql
+}
 
-echo "-- Nuking and restarting container"
-docker kill timescaledb
-docker rm timescaledb
-docker run -d --name timescaledb -p 127.0.0.1:5432:5432 -e POSTGRES_PASSWORD=password timescale/timescaledb:latest-pg12
+restart_container() {
+  echo "-- Nuking and restarting container"
+  docker-compose rm --stop --force -v
+  docker-compose up --build --detach
+}
 
-echo "-- Waiting for postgres server" 
-RETRIES=5
-until PGPASSWORD=password psql -h localhost -U postgres -p 5432 -c "select 1" > /dev/null 2>&1 || [ $RETRIES -eq 0 ]; do
-  echo "$((RETRIES--)) remaining attempts..."
-  sleep 1
-done
+wait_for_pg() {
+  echo "-- Waiting for postgres server" 
+  RETRIES=5
+  until docker exec seeries_db_1 psql -U seeries -c "select time from tempest limit 1" > /dev/null 2>&1 || [ $RETRIES -eq 0 ]; do
+    echo "$((RETRIES--)) remaining attempts..."
+    sleep 1
+  done
+}
 
-echo "-- Creating schema"
-PGPASSWORD=password psql -h localhost -p 5432 -U postgres -f src/sql/CREATE.sql 
-
-for f in bootstrap/generated_sql/*.sql ; do
-  echo "-- Loading $f"
-  PGPASSWORD=password psql -h localhost -p 5432 -U postgres -f $f
-done
+generate_sql
+restart_container
+wait_for_pg
