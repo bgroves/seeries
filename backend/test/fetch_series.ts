aef25228258e8a5cfcbcba3d339b06899c135024
Charlie Groves <cgroves@fitbit.com>
2020-10-18 17:29:40 -0400

    Improve validation, test missing data ranges (#26)
    Add validation over remaining input parameters and catch some nonsense requests like ends before the start.
    
    Delete some sensor push office data and use those gaps to run some tests.

--- backend/test/fetch_series.ts
+++ backend/test/fetch_series.ts
@@ -16,10 +16,86 @@ const baseParams = {
 
 test('Fetch simple series', async () => {
     const resp = await axios.get('http://localhost:8000/series', { params: baseParams });
+    assert.strictEqual(resp.data.length, 1);
     const segment = resp.data[0];
     assert.strictEqual(segment.start, baseParams.start);
     assert.strictEqual(segment.end, baseParams.end);
     assert.strictEqual(segment.points.length, baseParams.points);
+});
+
+test('Fetch completely missing range', async () => {
+    const params = { ...baseParams };
+    params.start = "2020-10-03T00:00:00.000Z";
+    params.end = "2020-10-03T00:10:00.000Z";
+    const resp = await axios.get('http://localhost:8000/series', { params: params });
+    assert.strictEqual(resp.data.length, 0);
+});
+
+test('Fetch same start and end', async () => {
+    const params = { ...baseParams };
+    params.end = params.start
+    const resp = await axios.get('http://localhost:8000/series', { params: params });
+    assert.strictEqual(resp.data.length, 0);
+});
+
+test('Fetch half missing range', async () => {
+    const params = { ...baseParams };
+    params.start = "2020-10-03T00:00:00.000Z";
+    params.end = "2020-10-03T00:20:00.000Z";
+    const resp = await axios.get('http://localhost:8000/series', { params: params });
+    assert.strictEqual(resp.data.length, 1);
+    const segment = resp.data[0];
+    assert.strictEqual(segment.start, "2020-10-03T00:10:00.000Z");
+    assert.strictEqual(segment.end, params.end);
+    assert.strictEqual(segment.points.length, 5);
+});
+
+test('Fetch gappy range', async () => {
+    const params = { ...baseParams };
+    params.start = "2020-10-03T01:00:00.000Z";
+    params.end = "2020-10-03T01:10:00.000Z";
+    const resp = await axios.get('http://localhost:8000/series', { params: params });
+    assert.strictEqual(resp.data.length, 5);
+    const firstSegment = resp.data[0];
+    assert.strictEqual(firstSegment.start, "2020-10-03T01:01:00.000Z");
+    assert.strictEqual(firstSegment.end, "2020-10-03T01:02:00.000Z");
+    assert.strictEqual(firstSegment.points.length, 1);
+    const lastSegment = resp.data[resp.data.length - 1];
+    assert.strictEqual(lastSegment.start, "2020-10-03T01:09:00.000Z");
+    assert.strictEqual(lastSegment.end, params.end);
+    assert.strictEqual(lastSegment.points.length, 1);
+});
+
+test('Fetch gappy range at low resolution', async () => {
+    const params = { ...baseParams };
+    params.start = "2020-10-03T01:00:00.000Z";
+    params.end = "2020-10-03T02:00:00.000Z";
+    const resp = await axios.get('http://localhost:8000/series', { params: params });
+    assert.strictEqual(resp.data.length, 1);
+    const firstSegment = resp.data[0];
+    assert.strictEqual(firstSegment.start, params.start);
+    assert.strictEqual(firstSegment.end, params.end);
+    assert.strictEqual(firstSegment.points.length, 10);
+});
+
+test('Fetch small range at excessively high resolution', async () => {
+    const params = { ...baseParams };
+    params.points = 16_384;
+    const resp = await axios.get('http://localhost:8000/series', { params: params });
+    assert.strictEqual(resp.data.length, 30);
+    assert.strictEqual(params.start, resp.data[0].start);
+});
+
+test('Fetch large series', async () => {
+    const params = { ...baseParams };
+    params.points = 16_384;
+    params.start = "2020-09-01T00:00:00.000Z";
+    params.end = "2020-10-01T00:00:00.000Z";
+    const resp = await axios.get('http://localhost:8000/series', { params: params });
+    const segment = resp.data[0];
+    assert.strictEqual(segment.start, params.start);
+    assert.strictEqual(segment.end, params.end);
+    assert.strictEqual(segment.points.length, params.points);
 });
 
 test('Leave out params', async () => {
@@ -57,7 +133,47 @@ test('Unknown aggregation', async () => {
         assert.ok((error.response.data as string).indexOf("AVG") != -1);
         return;
     }
-    assert.fail("Expected request with lots of points to raise a 400");
+    assert.fail("Expected unknown aggregation to raise a 400");
+});
+
+test('Unknown device', async () => {
+    const params = { ...baseParams };
+    params.device_name = "; DROP TABLES;";
+    try {
+        await axios.get('http://localhost:8000/series', { params: params });
+    } catch (error: any) {
+        assert.strictEqual(error.response.status, 400);
+        assert.ok((error.response.data as string).indexOf("DROP TABLES") != -1);
+        return;
+    }
+    assert.fail("Expected unknown device_name to raise a 400");
+});
+
+test('Wrong sensor for device type', async () => {
+    const params = { ...baseParams };
+    params.sensor = "; DROP TABLES;";
+    try {
+        await axios.get('http://localhost:8000/series', { params: params });
+    } catch (error: any) {
+        assert.strictEqual(error.response.status, 400);
+        assert.ok((error.response.data as string).indexOf("DROP TABLES") != -1);
+        return;
+    }
+    assert.fail("Expected request for a sensorpush device with a tempest-only sensor to raise a 400");
+});
+
+test('End before start', async () => {
+    const params = { ...baseParams };
+    params.end = params.start;
+    params.start = baseParams.end;
+    try {
+        await axios.get('http://localhost:8000/series', { params: params });
+    } catch (error: any) {
+        assert.strictEqual(error.response.status, 400);
+        assert.ok((error.response.data as string).indexOf("is after end") != -1);
+        return;
+    }
+    assert.fail("Expected request for a sensorpush device with a tempest-only sensor to raise a 400");
 });
 
 import '../src/index';
---
+++ bootstrap/sensorpush/README.md
@@ -0,0 +1,3 @@
+Data from a few Sensor Push sensors from late August to early October 2020. The data is in Eastern time, so shift it by 4 hours to get UTC.
+
+The office sensor has had the first 10 minutes of October 3rd in UTC deleted along with every other minute for 1:00 through 1:09 in UTC on the same day. The gaps there are used for testing the backend.
