7027af344068c14cc63f0f6b3afd145756ec5938
Charlie Groves <cgroves@fitbit.com>
2020-12-29 11:13:44 -0500

    Use a shared top-level tsconfig

---
+++ backend/.eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+    root: true,
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        tsconfigRootDir: __dirname, 
+        project: ['*/tsconfig.json']
+    },
+    plugins: [
+      '@typescript-eslint',
+    ],
+    extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:@typescript-eslint/recommended-requiring-type-checking',
+      'prettier',
+      'prettier/@typescript-eslint'
+    ],
+    ignorePatterns: [
+      "build",
+      ".eslintrc.js"
+    ]
+  };
