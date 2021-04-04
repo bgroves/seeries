3c0dfbcf09859497a21ccd42240be01de8133957
Ben Groves <groves.ben@gmail.com>
2021-04-03 20:34:51 -0400

    Add CORS middleware and matching .env config. (#39)

--- backend/api/src/index.ts
+++ backend/api/src/index.ts
@@ -3,16 +3,26 @@ import Router from "express-promise-router";
 import { fetchSeries } from "./handlers";
 import { validationErrorHandler } from "./validators";
 import { end } from "../../shared/src/db";
+import { requireIntEnv, requireStrEnv } from "../../shared/src/config";
+import cors from "cors";
+
+const apiPort = requireIntEnv("API_PORT");
+const corsOrigin = requireStrEnv("CORS_ORIGIN");
 
 const router = Router();
 // eslint-disable-next-line @typescript-eslint/no-misused-promises
 router.get("/series", fetchSeries);
 
 const app = express();
+app.use(
+  cors({
+    origin: corsOrigin == "*" ? "*" : `${corsOrigin}:${apiPort}`,
+  })
+);
 app.use(router);
 app.use(validationErrorHandler);
-const PORT = 8000;
-export const server = app.listen(PORT, () => {
-  console.log(`🐇[series]: Server is hopping at https://localhost:${PORT}`);
+
+export const server = app.listen(apiPort, () => {
+  console.log(`🐇[series]: Server is hopping at https://localhost:${apiPort}`);
 });
 server.addListener("close", end);
